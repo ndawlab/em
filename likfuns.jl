9ce7c7b1ec71d8629b86ba6e01853e346cf0487f
Yoel Sanchez Araujo <yoelsanchez006@gmail.com>
2018-04-25 12:52:46 -0400

    Update likfuns.jl

--- likfuns.jl
+++ likfuns.jl
@@ -5,25 +5,25 @@ function qlik(params,data)
 	beta = params[1]
 	lr = 0.5 + 0.5 * erf(params[2] / sqrt(2))
 
-	# It is important that we specify the type of Q below, as corresponding to the type 
+	# It is important that we specify the type of Q below, as corresponding to the type
 	# of the input parameters
-	# This allows the function to work with the algebraic differentiation 
+	# This allows the function to work with the algebraic differentiation
 	# the principle ("type stability") is that any variable that holds a function of the input params
 	# (here Q and ultimately lik)
-	# has to be of the same type as the input 
+	# has to be of the same type as the input
 
-	Q = zeros(typeof(beta),2)  
+	Q = zeros(typeof(beta),2)
 
 	lik = 0
-	
+
 	r = data[:r]
 	c = data[:c]
-	
+
 	for i = 1:length(c)
 		if (c[i]>0)
-    
+
 			lik += beta*Q[c[i]] - log(sum(exp.(beta*Q)))
-	
+
 			Q[c[i]] = (1-lr) * Q[c[i]] + r[i]
 		end
 	end
@@ -38,27 +38,27 @@ function jianlik(params,data)
 	beta = 2 * params[1] # match scaling with qlik
 	nu = 0.5 + 0.5 * erf(params[2] / sqrt(2))
 
-	# It is important that we specify the type of Q & lr below, as corresponding to the type 
+	# It is important that we specify the type of Q & lr below, as corresponding to the type
 	# of the input parameters
-	# This allows the function to work with the algebraic differentiation 
+	# This allows the function to work with the algebraic differentiation
 	# the principle ("type stability") is that any variable that holds a function of the input params
 	# (here Q, lr, and ultimately lik)
-	# has to be of the same type as the input 
+	# has to be of the same type as the input
 
-	Q = zeros(typeof(beta),2)  
+	Q = zeros(typeof(beta),2)
 
 	lr = convert(typeof(beta),1);
 
 	lik = 0
-	
+
 	r = data[:r]
 	c = data[:c]
 
 	for i = 1:length(c)
 		if (c[i]>0)
-    
+
 			lik += beta*Q[c[i]] - log(sum(exp.(beta*Q)))
-	
+
 			delta = r[i] - Q[c[i]]
 			Q[c[i]] +=  lr/2 * delta
 			lr = (1-nu) * lr + nu * abs(delta)
@@ -73,8 +73,8 @@ function seqlik(params,data)
 	beta1t1 = params[3]
 	beta2 = params[4]
  	lr = 0.5 + 0.5 * erf(params[5] / sqrt(2)) # (this is unit normal cdf
- 											  # so squashes any input to 0-1 and 
- 											  # transforms unit normal prior to uniform prior) 
+ 											  # so squashes any input to 0-1 and
+ 											  # transforms unit normal prior to uniform prior)
 	ps = params[6]
 
 	c1 = data[:ch1]
@@ -82,17 +82,17 @@ function seqlik(params,data)
 	r = data[:mn]
 	s = data[:st]
 
-	# It is important that we specify the type of the Qs below, as corresponding to the type 
+	# It is important that we specify the type of the Qs below, as corresponding to the type
 	# of the input parameters
-	# This allows the function to work with the algebraic differentiation 
+	# This allows the function to work with the algebraic differentiation
 	# the principle ("type stability") is that any variable that holds a function of the input params
 	# (here the Qs and ultimately lik)
-	# has to be of the same type as the input 
+	# has to be of the same type as the input
 
 	Q0 = zeros(typeof(beta1m),3,2)
 	Q1 = zeros(typeof(beta1m),2)
 	Qm = zeros(typeof(beta1m),2)
-	
+
 
 	lik = 0
 
@@ -101,22 +101,22 @@ function seqlik(params,data)
 	for i = 1:length(c1)
 		if (c1[i]>0)
 			# this uses a differentiable approximation to the max over the two actions at each state
-			Qm = [softmaximum(Q0[2,1],Q0[2,2]),softmaximum(Q0[3,1],Q0[3,2])] 
+			Qm = [softmaximum(Q0[2,1],Q0[2,2]),softmaximum(Q0[3,1],Q0[3,2])]
 			Qd = beta1m * Qm + beta1t0 * vec(Q0[1,:]) + beta1t1 * Q1
-			
+
 			if prevc>0
 				Qd[prevc] += ps
 			end
-    
+
 			lik += Qd[c1[i]] - log(sum(exp.(Qd)))
-    
+
 			if (c2[i]>0)
 				lik += beta2 * Q0[s[i],c2[i]] - log(sum(exp.(beta2 * Q0[s[i],:])))
-				
+
 				Q0[1,c1[i]] = (1-lr) * Q0[1,c1[i]] + Q0[s[i],c2[i]]
-				Q1[c1[i]] = (1-lr) * Q1[c1[i]] + r[i]	
+				Q1[c1[i]] = (1-lr) * Q1[c1[i]] + r[i]
 				Q0[s[i],c2[i]] = (1-lr) * Q0[s[i],c2[i]] + r[i]
-			end		
+			end
 		end
 		prevc = c1[i]
 	end
@@ -131,21 +131,21 @@ function simq(params, ntrials)
 	beta = params[1]
 	#lr = 1/(1+exp(-params[2]))
 	lr = 0.5 + 0.5 * erf(params[2] / sqrt(2))
-	
+
 	c = zeros(Int64,ntrials)
 	r = zeros(Int64,ntrials)
-	
+
 	Q = zeros(2)
 
 	for i = 1:ntrials
-    
+
 		ps = exp.(beta*Q)
 		ps = ps / sum(ps)
 		c[i] = (rand() > ps[1]) + 1
-			
+
 		r[i] = 2*round(rand()) - 1
 		Q[c[i]] = (1-lr) * Q[c[i]] + r[i]
-	end	
+	end
 	return (c,r)
 end
 
@@ -168,17 +168,17 @@ function simseq(params, ntrials)
 	Q0 = zeros(typeof(beta1m),3,2)
 	Q1 = zeros(typeof(beta1m),2)
 	Qm = zeros(typeof(beta1m),2)
-	
+
 	prevc = 0
-	
+
 	for i = 1:ntrials
 		Qm = [softmaximum(Q0[2,1],Q0[2,2]),softmaximum(Q0[3,1],Q0[3,2])]
 		Qd = beta1m * Qm + beta1t0 * vec(Q0[1,:]) + beta1t1 * Q1
-			
+
 		if prevc>0
 			Qd[prevc] += ps
 		end
-    
+
 		cp = exp.(beta2*Qd)
 		cp = cp / sum(cp)
 		c1[i] = (rand() > cp[1]) + 1
@@ -187,11 +187,11 @@ function simseq(params, ntrials)
 		cp = exp.(beta2*Q0[s[i],:])
 		cp = cp / sum(cp)
 		c2[i] = (rand() > cp[1]) + 1
-			
+
 		r[i] = 2*round(rand()) - 1
 
 		Q0[1,c1[i]] = (1-lr) * Q0[1,c1[i]] + Q0[s[i],c2[i]]
-		Q1[c1[i]] = (1-lr) * Q1[c1[i]] + r[i]	
+		Q1[c1[i]] = (1-lr) * Q1[c1[i]] + r[i]
 		Q0[s[i],c2[i]] = (1-lr) * Q0[s[i],c2[i]] + r[i]
 
 		prevc = c1[i]
@@ -202,23 +202,23 @@ end
 function simjian(params, ntrials)
 	beta = 2 * params[1]
 	nu = 0.5 + 0.5 * erf(params[2] / sqrt(2))
-	
+
 	c = zeros(Int64,ntrials)
 	r = zeros(Int64,ntrials)
-	
+
 	Q = zeros(2)
 	lr = 1
 
 	for i = 1:ntrials
-    
+
 		ps = exp(beta*Q)
 		ps = ps / sum(ps)
 		c[i] = (rand() > ps[1]) + 1
-			
+
 		r[i] = 2*round(rand()) - 1
 		delta = r[i] - Q[c[i]]
 		Q[c[i]] +=  lr/2 * delta
 		lr = (1-nu) * lr + nu * abs(delta)
-	end	
+	end
 	return (c,r)
 end
